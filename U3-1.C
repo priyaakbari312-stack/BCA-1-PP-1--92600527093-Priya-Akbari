e5f40ee124febf4cd6f9a48f001cbf1f721f6077
priyaakbari312-stack <priyaakbari312@gmail.com>
2026-08-07 09:50:51 +0530

    Add files via upload

---
+++ U3-1.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+	 int a,b;
+	 clrscr();
+
+	 printf("\n Enter value of a,b: ");
+	 scanf("%d%d",&a,&b);
+
+	 if(a > b)
+	 {
+		printf(" \n a number is maximum");
+	 }
+	 else
+	 {
+		 printf("\n b number is maximum");
+		 }
+		 getch();
+}
