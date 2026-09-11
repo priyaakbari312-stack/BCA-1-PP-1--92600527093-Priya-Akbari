00aa180c9dd15c6f29e3a1fcb681fdbb15e886db
priyaakbari312-stack <priyaakbari312@gmail.com>
2026-09-11 10:08:08 +0530

    Add files via upload

---
+++ SYMBOLIC.C
@@ -0,0 +1,25 @@
+//global variable demo
+#include<stdio.h>
+#include<conio.h>
+
+#define PI 3.14
+#define pf printf
+void main()
+{
+	  float r=2.5,a;
+	  clrscr();
+	  a = PI * r * r;
+	  pf("%f",a);
+
+	  getch();
+}
+
+
+
+
+
+
+
+
+
+
